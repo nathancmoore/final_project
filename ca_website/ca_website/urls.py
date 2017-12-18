1bbe72efee66cea7bf760ca0c392726630627766
maxawolff <maxawolff@hotmail.com>
2017-12-18 09:57:21 -0800

    added basic urls

--- ca_website/ca_website/urls.py
+++ ca_website/ca_website/urls.py
@@ -4,5 +4,12 @@ from django.conf.urls import url
 from django.contrib import admin
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
+    url(r'^admin/$', admin.site.urls),
+    url(r'^$', ),
+    url(r'^about-ca/',),
+    url(r'^steps-traditions', ),
+    url(r'^meeting-schedule', ),
+    url(r'^events$', ),
+    url(r'^events/item/(?P<pk>\d+)', ),
+    url(r'^internal-resources', ),
 ]
