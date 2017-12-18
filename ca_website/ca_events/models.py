6c451f652f339eec25180afb381b06c0aafd2461
Nathan Moore <ncmoore1986@gmail.com>
2017-12-18 15:11:35 -0800

    added service_meetings.json, added a ServiceMeeting model, cleaned up other models

--- ca_website/ca_events/models.py
+++ ca_website/ca_events/models.py
@@ -1,10 +1,13 @@
+"""Models for WSCA Website."""
+
+
 from django.db import models
 from multiselectfield import MultiSelectField
 
 
-# Create your models here.
 class Meeting(models.Model):
-    """Meeting model for ca site."""
+    """Meeting model for CA site."""
+
     district = models.IntegerField()
     meeting_name = models.CharField(max_length=50)
     weekday_choices = [
@@ -29,12 +32,12 @@ class Meeting(models.Model):
     duration = models.CharField(max_length=200)
     meeting_format_choices = [
         ('BB', 'Big Book Study'),
-        ('C', 'Closed meeting for those who disire to stop using cocaine and all other substances. Newcommers welcome.'),
-        ('CL', 'Candle Light'),
+        ('C', 'Closed meeting for those with a desire to stop using cocaine and all other substances. Newcomers welcome.'),
+        ('CL', 'Candlelight'),
         ('LS', 'Literature Study'),
-        ('M', 'Mend only'),
+        ('M', 'Men only'),
         ('NC', 'No children please.'),
-        ('SB', 'Non smoking with break.'),
+        ('SB', 'Meeting has a smoke break.'),
         ('W', 'Women only.'),
         ('HC', 'Wheelchair accessible'),
     ]
@@ -46,8 +49,25 @@ class Meeting(models.Model):
     last_updated = models.DateTimeField(auto_now=True)
 
 
+class ServiceMeeting(models.Model):
+    """Service Meeting model for CA site."""
+
+    meeting_name = models.CharField(max_length=50)
+    meeting_day = models.CharField(max_length=100)
+    start_time = models.DateTimeField()
+    location_name = models.CharField(max_length=150)
+    street = models.CharField(max_length=250)
+    suite = models.CharField(max_length=150)
+    city = models.CharField(max_length=150)
+    zip_code = models.IntegerField()
+    room = models.CharField(max_length=100)
+    notes = models.TextField()
+    last_updated = models.DateTimeField(auto_now=True)
+
+
 class Events(models.Model):
-    """Event model for ca site."""
+    """Event model for CA site."""
+
     event_photo = models.ImageField(upload_to='')
     event_name = models.CharField(max_length=50)
     weekday_choices = [
@@ -72,12 +92,12 @@ class Events(models.Model):
     duration = models.CharField(max_length=200)
     meeting_format_choices = [
         ('BB', 'Big Book Study'),
-        ('C', 'Closed meeting for those who disire to stop using cocaine and all other substances. Newcommers welcome.'),
+        ('C', 'Closed meeting for those with a desire to stop using cocaine and all other substances. Newcomers welcome.'),
         ('CL', 'Candle Light'),
         ('LS', 'Literature Study'),
-        ('M', 'Mend only'),
+        ('M', 'Men only'),
         ('NC', 'No children please.'),
-        ('SB', 'Non smoking with break.'),
+        ('SB', 'Meeting has a smoke break.'),
         ('W', 'Women only.'),
         ('HC', 'Wheelchair accessible'),
     ]
@@ -90,14 +110,16 @@ class Events(models.Model):
 
 
 class ExternalResources(models.Model):
-    """Resource model for ca site."""
+    """Resource model for CA site."""
+
     name = models.CharField(max_length=150)
     description = models.TextField(max_length=500, blank=True, default='')
     link = models.URLField()
 
 
 class InternalResources(models.Model):
-    """Resource model for ca site."""
+    """Resource model for CA site."""
+
     name = models.CharField(max_length=150)
     description = models.TextField(max_length=500, blank=True, default='')
     upload = models.FileField(upload_to='')
