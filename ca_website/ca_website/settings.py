789665793716ffcb83264a626e7e064e3556790d
maxawolff <maxawolff@hotmail.com>
2017-12-18 11:30:48 -0800

    adding bootstrap files to get bootstrap working correctly

--- ca_website/ca_website/settings.py
+++ ca_website/ca_website/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'ca_website'
+    'ca_website',
+    'bootstrap3',
 ]
 
 MIDDLEWARE = [
