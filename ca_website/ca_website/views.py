2d6cf4bd608287df5c15a7fbd09db8b57cb67302
Brian Wheeler <PoundsXD@localhost.localdomain>
2017-12-18 13:52:45 -0500

    Created framework for the views.

---
+++ ca_website/ca_website/views.py
@@ -0,0 +1,45 @@
+"""Detail views for site."""
+from django.views.generic import ListView, DetailView
+from django.shortcuts import render
+from django.views import view
+
+
+class HomeView(ListView):
+    """Display data for splash page."""
+    template_name = 'ca_website/home.html'
+    context_object_name = ''
+
+
+class About(ListView):
+    """Display about-ca page."""
+    template_name = 'ca_website/about.html'
+    context_object_name = ''
+
+
+class Steps(view):
+    """Display steps and traditions page."""
+    template_name = 'ca_website/steps.html'
+
+
+class Meetings(ListView):
+    """Display meeting schedule page."""
+    template_name = 'ca_website/meeting.html'
+    context_object_name = ''
+
+
+class Eventinfo(ListView):
+    """Display Event info page."""
+    template_name = 'ca_website/event.html'
+    context_object_name = ''
+
+
+class Eventdetail(DetailView):
+    """Display one event with details."""
+    template_name = 'ca_website/event_detail.html'
+    context_object_name = ''
+
+
+class Additional_resources(ListView):
+    """Display links to additional resources."""
+    template_name = 'ca_website/resource.html'
+    context_object_name = ''
