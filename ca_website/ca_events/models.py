d758b8e00ef9672b869c1d8f2231c7efd50ec416
kavdihodgson <kavdyjh@gmail.com>
2017-12-18 13:24:39 -0800

    finished models for events and meetings.

--- ca_website/ca_events/models.py
+++ ca_website/ca_events/models.py
@@ -1,25 +1,32 @@
 from django.db import models
 from multiselectfield import MultiSelectField
-import weekday_field
 
 
 # Create your models here.
 class Meeting(models.Model):
     """Meeting model for ca site."""
-    district = models.IntegerField(max_lenght=2)
-    meeting_name = models.Charfield(max_lenght=50)
-    weekday = weekday_field.fields.WeekdayField()
-    time = models.DateTimeField()
+    district = models.IntegerField()
+    meeting_name = models.CharField(max_length=50)
+    weekday_choices = [
+        ('sun', 'Sunday'),
+        ('mon', 'Monday'),
+        ('tue', 'Tuesday'),
+        ('wed', 'Wednesday'),
+        ('thu', 'Thursday'),
+        ('fri', 'Friday'),
+        ('sat', 'Saturday'),
+    ]
+    weekday = models.CharField(max_length=10, choices=weekday_choices)
+    start_time = models.DateTimeField()
     end_time = models.DateTimeField()
-    location_name = models.Charfield(max_lenght=150)
-    street = models.Charfield(max_lenght=250)
-    suite = models.Charfield(max_lenght=150)
-    city = models.Charfield(max_lenght=150)
-    state = models.Charfield(max_lenght=150)
-    zip_code = models.IntegerField(max_lenght=15)
-    room = models.Charfield(max_lenght=100)
+    location_name = models.CharField(max_length=150)
+    street = models.CharField(max_length=250)
+    suite = models.CharField(max_length=150)
+    city = models.CharField(max_length=150)
+    zip_code = models.IntegerField()
+    room = models.CharField(max_length=100)
     notes = models.TextField()
-    duration = models.Charfield(max_lenght=200)
+    duration = models.CharField(max_length=200)
     meeting_format_choices = [
         ('BB', 'Big Book Study'),
         ('C', 'Closed meeting for those who disire to stop using cocaine and all other substances. Newcommers welcome.'),
@@ -31,31 +38,66 @@ class Meeting(models.Model):
         ('W', 'Women only.'),
         ('HC', 'Wheelchair accessible'),
     ]
-    meeting_format = models.MultiSelectField(
-        max_lenght=200,
+    meeting_format = MultiSelectField(
+        max_length=200,
         choices=meeting_format_choices,
     )
-    accessiblility = models.Charfield(max_lenght=150)
+    accessiblility = models.CharField(max_length=150)
     last_updated = models.DateTimeField(auto_now=True)
 
 
+class Events(models.Model):
+    """Event model for ca site."""
+    event_photo = models.ImageField(upload_to='')
+    event_name = models.CharField(max_length=50)
+    weekday_choices = [
+        ('sun', 'Sunday'),
+        ('mon', 'Monday'),
+        ('tue', 'Tuesday'),
+        ('wed', 'Wednesday'),
+        ('thu', 'Thursday'),
+        ('fri', 'Friday'),
+        ('sat', 'Saturday'),
+    ]
+    weekday = models.CharField(max_length=10, choices=weekday_choices)
+    start_time = models.DateTimeField()
+    end_time = models.DateTimeField()
+    location_name = models.CharField(max_length=150)
+    street = models.CharField(max_length=250)
+    suite = models.CharField(max_length=150)
+    city = models.CharField(max_length=150)
+    zip_code = models.IntegerField()
+    room = models.CharField(max_length=100)
+    notes = models.TextField()
+    duration = models.CharField(max_length=200)
+    meeting_format_choices = [
+        ('BB', 'Big Book Study'),
+        ('C', 'Closed meeting for those who disire to stop using cocaine and all other substances. Newcommers welcome.'),
+        ('CL', 'Candle Light'),
+        ('LS', 'Literature Study'),
+        ('M', 'Mend only'),
+        ('NC', 'No children please.'),
+        ('SB', 'Non smoking with break.'),
+        ('W', 'Women only.'),
+        ('HC', 'Wheelchair accessible'),
+    ]
+    event_format = MultiSelectField(
+        max_length=200,
+        choices=meeting_format_choices,
+    )
+    accessiblility = models.CharField(max_length=150)
+    last_updated = models.DateTimeField(auto_now=True)
 
-# {
-#     "District": 3,
-#     "Meeting_Name": "Kenmore Keystone",
-#     "Weekday": "Monday",
-#     "Time": "7:30 PM",
-#     "End_Time": "8:30 PM",
-#     "Location_Name": "Church of the Redeemer",
-#     "Street": "6211 NE 182nd St",
-#     "Suite": "",
-#     "City": "Kenmore",
-#     "State": "WA",
-#     "Zip": 98029,
-#     "Room": "",
-#     "Notes": "",
-#     "Duration": 60,
-#     "Format": "CL, MM",
-#     "Accessibility": "",
-#     "Updated": "",
-# },
+
+class ExternalResources(models.Model):
+    """Resource model for ca site."""
+    name = models.CharField(max_length=150)
+    description = models.TextField(max_length=500, blank=True, default='')
+    link = models.URLField()
+
+
+class InternalResources(models.Model):
+    """Resource model for ca site."""
+    name = models.CharField(max_length=150)
+    description = models.TextField(max_length=500, blank=True, default='')
+    upload = models.FileField(upload_to='')
--- ca_website/ca_website/settings.py
+++ ca_website/ca_website/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'ca_website',
+    'ca_events',
 ]
 
 MIDDLEWARE = [
@@ -77,6 +79,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': os.environ.get('DATABASE_NAME', ''),
+        'USER': os.environ.get('USER', ''),
         'PASSWORD': os.environ.get('PASSWORD', ''),
         'HOST': 'localhost',
         'PORT': '5432',
