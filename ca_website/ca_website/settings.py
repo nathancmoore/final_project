7bb774b72c6d57bd5a1aa8cee4f1d147f9232e58
kavdihodgson <kavdyjh@gmail.com>
2017-12-18 10:49:09 -0800

    setting up files.

--- ca_website/ca_website/settings.py
+++ ca_website/ca_website/settings.py
@@ -75,11 +75,16 @@ WSGI_APPLICATION = 'ca_website.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.environ.get('DATABASE_NAME', ''),
+        'PASSWORD': os.environ.get('PASSWORD', ''),
+        'HOST': 'localhost',
+        'PORT': '5432',
+        'TEST': {
+            'NAME': 'TEST_DB',
+        },
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
