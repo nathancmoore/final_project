4c93a0ccc28f6b816cc713facf3f63fbfae7b344
maxawolff <maxawolff@hotmail.com>
2017-12-18 16:16:37 -0800

    removed temporary view

--- ca_website/ca_website/urls.py
+++ ca_website/ca_website/urls.py
@@ -7,7 +7,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^$', views.home_view, name='homepage'),
+    # url(r'^$', views.home_view, name='homepage'),
     # url(r'^about-ca/',),
     # url(r'^steps-traditions', ),
     # url(r'^meeting-schedule', ),
