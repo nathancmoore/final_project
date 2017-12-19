45c72545b2008954388dd4e5dd48c9b9c47d08b9
Brian Wheeler <PoundsXD@localhost.localdomain>
2017-12-18 19:26:02 -0500

    Modified urls and added context object names to class based views.

--- ca_website/ca_website/urls.py
+++ ca_website/ca_website/urls.py
@@ -2,18 +2,19 @@
 
 from django.conf.urls import url
 from django.contrib import admin
-from ca_website import views, settings
+from ca_website import settings
 from django.conf.urls.static import static
+from ca_website.views import HomeView, About, Steps, Meetings, Eventinfo, Eventdetail, Additionalresources
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^$', views.home_view, name='homepage'),
-    # url(r'^about-ca/',),
-    # url(r'^steps-traditions', ),
-    # url(r'^meeting-schedule', ),
-    # url(r'^events$', ),
-    # url(r'^events/item/(?P<pk>\d+)', ),
-    # url(r'^internal-resources', ),
+    url(r'^$', HomeView.as_view(), name='homepage'),
+    url(r'^about-ca/', About.as_view(), name='about'),
+    url(r'^steps-traditions', Steps.as_view(), name='steps'),
+    url(r'^meeting-schedule', Meetings.as_view(), name='meetings'),
+    url(r'^events$', Eventinfo.as_view(), name='eventinfo'),
+    url(r'^events/item/(?P<pk>\d+)', Eventdetail.as_view(), name='eventdetail'),
+    url(r'^internal-resources', Additionalresources.as_view(), name='resources'),
 ]
 
 if settings.DEBUG:
